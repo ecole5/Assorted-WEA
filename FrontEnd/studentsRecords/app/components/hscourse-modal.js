21dfa2755d084b915e524fbad973f6f1d166993d
Evan Cole <ecole5@uwo.ca>
2017-03-01 11:39:52 -0500

    Added the missing code

--- FrontEnd/studentsRecords/app/components/hscourse-modal.js
+++ FrontEnd/studentsRecords/app/components/hscourse-modal.js
@@ -18,6 +18,7 @@ export default Ember.Component.extend({
     // get all documents of type modelName
     this.set('codeModel', this.get('store').findAll("hscourse"));
       this.set('schoolModel', this.get('store').findAll("secondaryschool"));
+      this.set('subjectModel', this.get('store').findAll("hssubject"));
   },
 
 
@@ -35,7 +36,24 @@ export default Ember.Component.extend({
         current.save();
     },
 
-   
+   //edit a code option (sometimes we have a race condition, where model is null and thus it wont set)
+    selectSubject(value) {
+      var current = this.get('currentModel');
+        current.set('subject', value);
+        current.save();
+    },
+
+    editName(value) {
+      var current = this.get('currentModel');
+      
+      if (!value){
+         value = "NEW NAME";
+         alert("Name can not be empty!");
+      }
+        
+        current.set('name', value);
+        current.save();
+    },
     //Set context of what is being currently edited
     setCurrentModel(model) {
       this.set('currentModel', model);
