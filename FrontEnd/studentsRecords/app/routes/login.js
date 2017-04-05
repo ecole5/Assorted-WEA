a433c77ba2eefcaebdd1193f606c9f0f0372cbba
Evan Cole <ecole5@uwo.ca>
2017-04-04 20:03:41 -0400

    Cleaned up oudauth ui, fixed program admin modal

--- FrontEnd/studentsRecords/app/routes/login.js
+++ FrontEnd/studentsRecords/app/routes/login.js
@@ -2,7 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   renderTemplate: function () {
-    if (this.get('oudaAuth').get('isAuthenticated')) { //This is to disable the effect of back button in the browser
+
+     this.get('oudaAuth').close();
+      this.render('login', {  // the template to render
+        into: 'application' ,  // the template to render into
+        outlet: 'login'
+      });
+
+
+    /*(if (this.get('oudaAuth').get('isAuthenticated')) { //This is to disable the effect of back button in the browser
       //     location.replace(location.origin+'/home');
       this.get('oudaAuth').close();
       this.render('login', {  // the template to render
@@ -14,6 +22,6 @@ export default Ember.Route.extend({
         into: 'application' ,  // the template to render into
         outlet: 'login'
       });
-    }
+    }*/
   }
 });
