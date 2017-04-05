fa80c426563058f4c801f06636bad7259d3e147a
Evan Cole <ecole5@uwo.ca>
2017-04-05 01:19:31 -0400

    Adjudication is near fully functional

--- FrontEnd/studentsRecords/app/components/perform-adjudication.js
+++ FrontEnd/studentsRecords/app/components/perform-adjudication.js
@@ -20,8 +20,10 @@ export default Ember.Component.extend({
 
 
   processing: false,
+ complete: false,
   selectedTerm: null,
   result: null,
+
 
 
   init() {
@@ -32,11 +34,12 @@ export default Ember.Component.extend({
     this.get('store').findAll('term').then(function (records) {
       self.set('termModel', records);
 
-    
+
 
     });
 
   },
+
 
 
 
@@ -53,37 +56,39 @@ export default Ember.Component.extend({
     },
     go() {
       this.set('processing', true);
+      this.set('complete', false)
       var self = this;
 
       this.get('store').findAll('category').then(function (records) {
         self.set('categoryModel', records);
         self.get('store').findAll('faculty').then(function (records) {
-            self.set('facultyModel', records);
-        self.get('store').findAll('grade').then(function (records) {
-          self.set('gradeModel', records);
-          self.get('store').findAll('comment').then(function (records) {
-            self.set('commentModel', records);
-            self.get('store').findAll('catcomment').then(function (records) {
-              self.set('catcommentModel', records);
-              self.get('store').query('student', {
-                all: 'true',
-              }).then(function (records) {
-                self.set('studentModel', records);
-              
-                self.get('store').findAll('plan').then(function (records) {
-                  self.set('planModel', records);
-                  self.get('store').findAll('course').then(function (records) {
-                    self.set('courseModel', records);
-                    self.get('store').findAll('program').then(function (records) {
-                      self.set('programModel', records);
+          self.set('facultyModel', records);
+          self.get('store').findAll('grade').then(function (records) {
+            self.set('gradeModel', records);
+            self.get('store').findAll('comment').then(function (records) {
+              self.set('commentModel', records);
+              self.get('store').findAll('catcomment').then(function (records) {
+                self.set('catcommentModel', records);
+                self.get('store').query('student', {
+                  all: 'true',
+                }).then(function (records) {
+                  self.set('studentModel', records);
 
-                      self.get('store').findAll('rule').then(function (records) {
-                        self.set('ruleModel', records);
-                        self.get('store').findAll('rulecomment').then(function (records) {
-                          self.set('rulecommentModel', records);
+                  self.get('store').findAll('plan').then(function (records) {
+                    self.set('planModel', records);
+                    self.get('store').findAll('course').then(function (records) {
+                      self.set('courseModel', records);
+                      self.get('store').findAll('program').then(function (records) {
+                        self.set('programModel', records);
+
+                        self.get('store').findAll('rule').then(function (records) {
+                          self.set('ruleModel', records);
+                          self.get('store').findAll('rulecomment').then(function (records) {
+                            self.set('rulecommentModel', records);
                             self.send('startLoop');
-                        });
+                          });
 
+                        });
                       });
                     });
                   });
@@ -93,233 +98,177 @@ export default Ember.Component.extend({
           });
         });
       });
-      });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     },
 
     startLoop() {
-      
-    
 
-      var student = this.get('studentModel');
+
+
+      var students = this.get('studentModel');
       var self = this;
-      
 
       //find a student
-      student.forEach(function(student) {
+      for (var i = 0; i < students.get('length'); i++) {
 
-      //Create record
-         //Create the adjudication record
-          var myStore = self.get('store');
-          var myAdjudication = myStore.createRecord("adjudication", {
-          date: new Date(),
-          termAVG: null,
-          unitPassed: null,
-          unitTotal: null,
-          note: null,
-          program: null,
-          plan: null,
-          term: self.get('selectedTerm'),
-          student: student,
+        //Set a reference to the current student 
+        var student = students.objectAt(i);
 
-  
-      });
-
-      self.set('result',myAdjudication);
-
-
-        
-          
-        
-        //Find all relevent grades
+        //Find all relevent grades in that semester
         var gradesStudent = student.get('grades');
         var termGrades = [];
-        gradesStudent.forEach(function(grade){
-          if (grade.get('term').get('id') === self.get('selectedTerm').get('id')){
-                termGrades.push(grade); 
-          }   
+        gradesStudent.forEach(function (grade) {
+          if (grade.get('term').get('id') === self.get('selectedTerm').get('id')) {
+            termGrades.push(grade);
+          }
         }); //end of determaining term grade
-        
+
+        //Check that enroledd in semester before continuing
+        if (termGrades.length === 0) {
+          console.log("student not in semester");
+          continue; //stop before evaluating the sutdent anymore
+        }
+
+
         //Calculate load and YWA
         var load = 0;
         var total = 0;
-         termGrades.forEach(function(grade){
-          
-            var temp = parseInt(grade.get('course').get('unit'));
-             total = parseInt(grade.get('mark')) / temp;
-             load = load + temp;
-        
+        var passed = 0;
+        termGrades.forEach(function (grade) {
+          var temp = parseInt(grade.get('course').get('unit'));
+          var temp2 = parseInt(grade.get('mark'));
+          if (temp2 > 50) {
+            passed = passed + temp;
+          }
+          total = temp2 / temp;
+          load = load + temp;
+
         });
 
-        var ywa = total/load;
-        //End of calculation of YWA
+        var ywa = total / load;
 
-
-        
 
         //Determain level and plan
-         if (termGrades[0]) {
-         var program = termGrades[0].get('program');
-         var level = program.get('level'); //should be same
-          var plan = termGrades[0].get('plan'); //could be diffrent but we out of time
-
-          
-          myAdjudication.set('plan',plan);
-          myAdjudication.set('program', 'program');
-          myAdjudication.set('termAVG', ywa);
-          myAdjudication.set('unitToal', total);
-          myAdjudication.save();
-        
+        var program = termGrades[0].get('program');
+        var level = program.get('level'); //should be same
+        var plan = termGrades[0].get('plan'); //could be diffrent but we out of time
 
 
-          //Evaluate all the independent categories
-          var category = [];
-          category = plan.get('faculty').get('category');
+        //Create the adjudication, comments will be created latter
+        var myStore = self.get('store');
+        var myAdjudication = myStore.createRecord("adjudication", {
+          date: new Date(),
+          termAVG: ywa.toString(),
+          unitPassed: passed.toString(),
+          unitTotal: total.toString(),
+          note: "This is a sample note",
+          program: self.get('store').peekRecord('program', program.get('id')),
+          plan: self.get('store').peekRecord('plan', plan.get('id')),
+          term: self.get('selectedTerm'),
+          student: self.get('store').peekRecord('student', student.get('id')),
 
-          category.forEach(function(cat){
-          var allRules= [];
-          if (cat.get('independent')){
+        });
 
-            //Get complete list of rules for category
-           
-            var catRules = [];
-            catRules = cat.get('rules');
-            catRules.forEach(function (rule){
-             
-              if (rule.get('type')){
-                  allRules.push(rule);
-                 
-              }
-              else if (rule.get('plan').get('id') === plan.get('id')){
-                allRules.push(rule);
-                
-              }
-              
-            }); //End of getting complete list of rule from category
-
-            var ruleFlag = 'test';
-            allRules.forEach(function (rule){
-              var exp = rule.get('log');
-              var exp2 = exp.replace('AVG', ywa) ;
-              var exp3 = exp2.replace ('LOAD',load);
-              var exp4 = exp3.replace ('YEAR',level);
-              
-              if (!(eval(exp4)))
-              {
-                ruleFlag = rule;
-              }
-              
-            });
-            
-              var commentsTotal = [];
-               var catComments = [];
-            //Get all the category comments
-            if (ruleFlag === 'test' && cat.get('allRules')){
-              
-      
-           
-              catComments = cat.get('comment');
-
-               catComments.forEach(function (comment){
-                var actualComment = comment.get('comment');
-                var newComment = myStore.createRecord("adjcomment", {
-          comment: actualComment,
-          adjudication: myAdjudication,
-  
-      });
-      newComment.save();
-
-            });
-
-            }
-            //Get all the category and rule commnents
-            else if (ruleFlag !== 'test'){
-           
-              commentsTotal = ruleFlag.get('comment');
-             
-              catComments = cat.get('comment');
-              
-
-              catComments.forEach(function(comment){
-                commentsTotal.push(comment);
-              });
-         
-
-               commentsTotal.forEach(function (comment){
-                var actualComment = comment.get('comment');
-                var newComment = myStore.createRecord("adjcomment", {
-          comment: actualComment,
-          adjudication: myAdjudication,
-  
-      });
-      newComment.save();
-
-            });
+        myAdjudication.save();
 
 
-            }
-                
-          }   //end of evaluating independents
-          else{
-            self.send('badAdjudication');
+        //Evaluate all the independent categories
+        var category = [];
+        category = plan.get('faculty').get('category');
+
+        var foundOne = false; //will be true when one of the none independent categories is satisfied
+
+
+
+        for (i = 0; i < category.get('length'); i++) {
+
+          var cat = category.objectAt(i);
+          var independentType = cat.get('independent'); //true if independent
+
+          //Wont evaluate if another none independent category has been meet
+          if (foundOne && !independentType) {
+            continue;
           }
-          
-        }); //end of independent loop
+
+          //Get complete list of rules for the category plan combo
+          var allRules = [];
+
+           cat.get('rules').forEach(function (rule) {
+
+            if (rule.get('type')) { //category specific rules
+              allRules.push(rule);
+
+            }
+            else if (rule.get('plan').get('id') === plan.get('id')) { //plan specific rules
+              allRules.push(rule);
+
+            }
+
+          }); 
+
+
+          //Check each rule and see if they satisfy ture
+          var allRuleSatisfied = true;
+          var oneRuleSatisfied = false;
+          allRules.forEach(function (rule) {
+            var exp = rule.get('log');
+            var exp2 = exp.replace('AVG', ywa);
+            var exp3 = exp2.replace('LOAD', load);
+            var exp4 = exp3.replace('YEAR', level);
+
+
+            if (!(eval(exp4))) {
+             allRuleSatisfied = false;
+            }
+            else {
+              oneRuleSatisfied = rule;
+            }
+
+          });
+
+
+          //Evaluate category satisfactory based on type and number of rules satisifed          
+          if (cat.get('allRules') && allRuleSatisfied) { //all rules must be true, and they are all true
+            console.log("All rules were true"); //get comment codes just for that specific rule
+
+            //Dont run for other none independent 
+            if (!independentType){
+              foundOne = true;
+            }
+
+          }
+
         
+          else if(!cat.get('allRules') && oneRuleSatisfied)  {  //just one rules needed and one rules true
+            console.log("Some of the rules were true"); //Get comment codes for both category and speicifc rule
 
-       
+              //Dont run for other none independent 
+              if (!independentType){
+              foundOne = true;
+            }
 
           
-
-
-         }
-         else{
-          
-            self.send('badAdjudication');
-            
-         }
-    
+          }
          
-          
 
 
+
+        } //end of looping through cats 
+
+      }//end of student loop
+    
   
-  myAdjudication.save();
-      }); //end of students loop
-    },
+    self.send('showComplete');
+  }, //end of entire action loop
+  
+showComplete(){
+  this.set("processing", false);
+  this.set("complete", true);
+
+},
 
 
-
-    badAdjudication(){
-      var mine = this.get('result');
-      mine.set('note', 'could not complete');
-      mine.save();
-      console.log('bad');
-    }
-
-
-
-
-  },
-
-
+  }//end of action
 
 });
 
